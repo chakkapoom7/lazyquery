1a55a08b4d8ce96f3626b22cbeda7a8d9a68edfe
chakkapoom7 <chakkapoom@gmail.com>
2019-05-17 12:45:18 +0700

    clean some code

--- lazyquery.php
+++ lazyquery.php
@@ -1,88 +1,79 @@
-<?php 
+<?php
 
-class DB {
-    private $config ;
-    private $conn ;
+class DB
+{
+    private $config;
+    private $conn;
 
-    function __construct( $configfile = './config.json' ) {
-        $this->config = json_decode( file_get_contents( $configfile ) , true )  ; 
+    public function __construct($configfile = './config.json')
+    {
+        $this->config = json_decode(file_get_contents($configfile), true);
 
-        if($this->config['dbtype'] === "mssql"){
+        if ($this->config['dbtype'] === "mssql") {
 
-            $connectionInfo = array( "Database"=>$this->config['dbname'], "UID"=>$this->config['username'], "PWD"=>$this->config['password'] );
-            $conn = sqlsrv_connect( $this->config['dburl'], $connectionInfo);
-            if( $conn === false ) {
-                die( print_r( sqlsrv_errors(), true));
+            $connectionInfo = array("Database" => $this->config['dbname'], "UID" => $this->config['username'], "PWD" => $this->config['password']);
+            $conn = sqlsrv_connect($this->config['dburl'], $connectionInfo);
+            if ($conn === false) {
+                die(print_r(sqlsrv_errors(), true));
             }
             $this->conn = $conn;
 
-        }elseif( $this->config['dbtype'] === "mysql" ){
+        } elseif ($this->config['dbtype'] === "mysql") {
 
-            $conn=mysqli_connect($this->config['dburl'],$this->config['username'],$this->config['password'],$this->config['dbname']);
-            if (mysqli_connect_errno())
-            {
-                die ( "Failed to connect to MySQL: " . mysqli_connect_error() );
+            $conn = mysqli_connect($this->config['dburl'], $this->config['username'], $this->config['password'], $this->config['dbname']);
+            if (mysqli_connect_errno()) {
+                die("Failed to connect to MySQL: " . mysqli_connect_error());
             }
             $this->conn = $conn;
+        } else {
+            die("configfile incorrect please check \"dbtype\" in configfile .");
         }
-
     }
 
-    public function curconf () {
+    public function curconf()
+    {
         return $this->config;
-    }    
+    }
 
-    public function query ( $sqlstr ) {
+    public function query($sqlstr)
+    {
 
-        if( $this->config['dbtype'] === "mssql" ){
+        if ($this->config['dbtype'] === "mssql") {
 
-            $stmt = sqlsrv_query( $this->conn , $sqlstr );
-            
-            if( $stmt === false ) {
-                // die(print_r(sqlsrv_errors(), true));
-                // sqlsrv_free_stmt($stmt);
-                return array("result"=>false,"data"=>sqlsrv_errors());
-            }else{
+            $stmt = sqlsrv_query($this->conn, $sqlstr);
+
+            if ($stmt === false) {
+                return array("result" => false, "data" => sqlsrv_errors());
+            } else {
                 $resdata = array();
-                while($row = sqlsrv_fetch_array( $stmt , SQLSRV_FETCH_ASSOC))
-                {
-                    array_push($resdata,$row);
+                while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+                    array_push($resdata, $row);
                 }
-                return  array("result"=>true, "data"=>$resdata ) ;
+                return array("result" => true, "data" => $resdata);
             }
 
-            // while($row = sqlsrv_fetch_array( $resault , SQLSRV_FETCH_ASSOC))
-            // {
-            // 	print_r($row);
-            // }
-
-        } elseif ( $this->config['dbtype'] === "mysql" ) {
+        } elseif ($this->config['dbtype'] === "mysql") {
             $resdata = array();
 
-            $result = mysqli_query( $this->conn , $sqlstr );
+            $result = mysqli_query($this->conn, $sqlstr);
 
             if (mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_assoc($result)) {
-                    array_push($resdata,$row);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($resdata, $row);
                 }
-                return  array("result"=>true, "data"=>$resdata ) ;
-             } else {
-                return  array("result"=>true, "data"=>array() ) ;
-             }
-            
+                return array("result" => true, "data" => $resdata);
+            } else {
+                return array("result" => true, "data" => array());
+            }
         }
-
     }
 
-    public function close ( ) {
-        if( $this->config['dbtype'] === "mssql" ){
+    public function close()
+    {
+        if ($this->config['dbtype'] === "mssql") {
             sqlsrv_close($this->conn);
-        } elseif ( $this->config['dbtype'] === "mysql" ) {
+        } elseif ($this->config['dbtype'] === "mysql") {
             mysqli_close($this->conn);
         }
     }
-
-} 
-
-
-?>
+}
